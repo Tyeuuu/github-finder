04e5a2d80cff4e9041eb0b601b3bb4aa55baab27
Tye Fender <tyefender@gmail.com>
2022-02-06 06:51:03 +1000

    initial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,18 @@
+function About() {
+  return (
+    <>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <p className="mb-4 text-2xl font-light">
+        A React app to search GitHub profiles and see profile details.
+      </p>
+      <p className="text-lg text-blue-400">
+        Version: <span>1.0.0</span>
+      </p>
+      <p className="text-lg text-green-400">
+        Layout By: <span>Tye Fender</span>
+      </p>
+    </>
+  );
+}
+
+export default About;
